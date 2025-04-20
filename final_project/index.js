ea4bcb49a10546937ba5ca057f280138aff71cb9
Zeynep Dundar <zeynepdundar@Zeyneps-MacBook-Pro.local>
2025-04-20 17:17:24 +0300

    5 steps complete

--- final_project/index.js
+++ final_project/index.js
@@ -8,13 +8,33 @@ const app = express();
 
 app.use(express.json());
 
+
+// disable authentication for a moment
 app.use("/customer",session({secret:"fingerprint_customer",resave: true, saveUninitialized: true}))
 
 app.use("/customer/auth/*", function auth(req,res,next){
-//Write the authenication mechanism here
-});
- 
-const PORT =5000;
+
+
+if (req.session.authorization){
+    let token = req.session.authorization['accessToken']
+    jwt.verify(token, "access", (err, user ) => {
+        if (!err){
+            req.user = user;
+            res.send.stats(200).json({message: "user is authenticated"});
+            // proceed to next middleware
+            next();
+        }else{
+            //
+            res.send.stats(403).json({message: "user is not authenticated"});
+        }
+    });
+}else{
+    // return error if no token is found for the user
+    return res.status(404).json({message:"User is not logged in"});
+}}
+);
+
+const PORT=5000;
 
 app.use("/customer", customer_routes);
 app.use("/", genl_routes);
--- final_project/router/general.js
+++ final_project/router/general.js
@@ -10,34 +10,56 @@ public_users.post("/register", (req,res) => {
   return res.status(300).json({message: "Yet to be implemented"});
 });
 
+
+// TODO: Sample get endpoint
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  return res.send(users, null, 4);
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  return res.send(books[1])
  });
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const author = req.params.author
+  if (author){
+    let filtered =  Object.fromEntries(Object.entries(books).filter(([_,value]) => value["author"]===author));
+    if (filtered){
+      return res.json({filtered});
+    }else{
+      return res.status(404).json({message: "No books found for this author"});
+    }
+  }
 });
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const title = req.params.title
+  if (title){
+    let filtered =  Object.fromEntries(Object.entries(books).filter(([_,value]) => value["title"]===title));
+    if (filtered){
+      return res.json({filtered});
+    }else{
+      return res.status(404).json({message: "No books found for this itle"});
+    }
+  }
 });
 
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const isbn = req.params.isbn
+  if (isbn && books[Number(isbn)] && books[Number(isbn)]["reviews"]){
+    const book_rev = books[Number(isbn)]["reviews"];
+    return res.json({book_rev})
+  }else{
+    return res.status(404).json({message: "ISBN invalid or no books found by this isbn"});
+  }
 });
 
 module.exports.general = public_users;
